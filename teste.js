881118c4e8e8c8fbb2c0637941a820a87d7d089e
luizotavio1337 <fofao8416@gmail.com>
2021-10-05 22:25:14 -0300

    luiz otavio

---
+++ teste.js
@@ -0,0 +1,19 @@
+let peso = 60;
+if ("peso > 50"){
+}
+  console.log ("a peça possui um peso adequado, pode ser cadastrada")
+
+let listadepecas = ["disco de freio ", "amortecedor", "vela ", "suspeção","radiador"]
+
+if (listadepecas.lenght <10 ){
+  console.log ("A lista ainda possui espaço")
+  } else {
+      console.log ("A lista não possui mais espaço")
+
+    }
+    let texto = "55" 
+    let numero = 55 
+
+    let nomepecas = "listapecas[0];" 
+    if (">3") {
+}
